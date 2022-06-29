0f74cc5ba14ef213e02c494bf28ed45a296c35fa
jhs <ghtla0330@naver.com>
2022-06-29 12:23:07 +0900

    20220629_4장 완료

--- src/main/java/com/sim/book/springboot/web/PostsApiController.java
+++ src/main/java/com/sim/book/springboot/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.sim.book.springboot.web;
 
 import com.sim.book.springboot.service.posts.PostsService;
+import com.sim.book.springboot.web.dto.PostsListResponseDto;
 import com.sim.book.springboot.web.dto.PostsResponseDto;
 import com.sim.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sim.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,8 +27,19 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id){
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
